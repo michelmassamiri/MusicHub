f92789ae25dd5d934f61e0da6b6ea028263335b2
michelmassamiri <massamiri.michel@gmal.com>
2018-12-08 18:42:44 +0100

    Authentication route

--- Back/routes/index.js
+++ Back/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+/* Auth Controller */
+var authController = require('../controllers/authentication');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/google', function(req, res, next) {
+    res.send('heloo word');
 });
 
+router.post('/google', authController.verifyGoogleToken);
 module.exports = router;
