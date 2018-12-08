0af4d03f62a1f18c58c0846683f09a65e5edb5b3
michelmassamiri <massamiri.michel@gmal.com>
2018-12-08 18:47:20 +0100

    ClientID config script

---
+++ Front/Front/src/app/socialloginConfig.ts
@@ -0,0 +1,10 @@
+import {  AuthServiceConfig, GoogleLoginProvider } from 'angular-6-social-login';
+
+export function getAuthServiceConfigs() {
+  const config = new AuthServiceConfig([{
+    id: GoogleLoginProvider.PROVIDER_ID,
+    provider: new GoogleLoginProvider('490320770076-pgociv0l4kfqvtufupkengo5clh8ha62.apps.googleusercontent.com')
+  }]);
+
+  return config;
+}
