4adf96fbcb60c418fc7c25891f2dc5946bd3893c
michelmassamiri <massamiri.michel@gmal.com>
2018-12-08 18:43:46 +0100

    Update the App.js

--- Back/app.js
+++ Back/app.js
@@ -3,23 +3,30 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+/* Body Parser and mongoose */
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+mongoose.connect('mongodb://localhost:27017/MusicHub', {useNewUrlParser: true});
+var db = mongoose.connection;
 
-app.use('/', indexRouter);
+app.use('/auth', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
