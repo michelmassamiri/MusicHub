5485ff484384fe099124c5488ef25a03a413bcf3
Michel Massamiri <massamiri.michel@gmail.com>
2018-12-10 00:24:30 +0100

    API for Authentication with google works

--- Back/routes/index.js
+++ Back/routes/index.js
@@ -4,10 +4,6 @@ var router = express.Router();
 /* Auth Controller */
 var authController = require('../controllers/authentication');
 
-/* GET home page. */
-router.get('/google', function(req, res, next) {
-    res.send('heloo word');
-});
+router.post('/google', authController.signInWithGoogle);
 
-router.post('/google', authController.verifyGoogleToken);
 module.exports = router;
