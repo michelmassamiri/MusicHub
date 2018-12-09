be0a9042a4bba8e13f4b79b6bb61e70aa8634c45
Michel Massamiri <massamiri.michel@gmail.com>
2018-12-10 00:27:29 +0100

    Refactoring for the routes path

--- Front/Front/src/app/app-routing.module.ts
+++ Front/Front/src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {HomeComponent} from './components/home/home.component';
+import {LoginComponent} from './components/login/login.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
