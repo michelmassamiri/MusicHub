3c7b140d8e336f55d4bbd0fe904f0f57d8d8e12c
Michel Massamiri <massamiri.michel@gmail.com>
2018-12-10 00:23:56 +0100

    Export properly IDclient

--- Back/oauth2config/oauth2client.js
+++ Back/oauth2config/oauth2client.js
@@ -1,1 +1,7 @@
-export const googleIdClient = '490320770076-pgociv0l4kfqvtufupkengo5clh8ha62.apps.googleusercontent.com';
+const socialsIdClient = {
+    googleIdClient: '490320770076-pgociv0l4kfqvtufupkengo5clh8ha62.apps.googleusercontent.com',
+    spotifyIdClient: '',
+    deezerIdClient: ''
+};
+
+module.exports.socialsIdClient = socialsIdClient;
