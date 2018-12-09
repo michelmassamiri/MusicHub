d35232f56591d7ccbaa81917afaf18fc84473a31
Michel Massamiri <massamiri.michel@gmail.com>
2018-12-10 00:27:43 +0100

    Update the App module

--- Front/Front/src/app/app.module.ts
+++ Front/Front/src/app/app.module.ts
@@ -2,19 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { AppRoutingModule } from './app-routing.module';
 
 import {SocialLoginModule, AuthServiceConfig} from 'angular-6-social-login';
-import {getAuthServiceConfigs} from "./socialloginConfig";
+import {getAuthServiceConfigs, tokenGetter} from "./socialloginConfig";
 import {HttpClientModule} from "@angular/common/http";
+import {JwtModule} from "@auth0/angular-jwt";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
+    LoginComponent,
     HeaderComponent,
     FooterComponent,
   ],
@@ -22,7 +23,14 @@ import {HttpClientModule} from "@angular/common/http";
     BrowserModule,
     AppRoutingModule,
     SocialLoginModule,
-    HttpClientModule
+    HttpClientModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetter,
+        whitelistedDomains: ['localhost:3000'],
+        blacklistedRoutes: ['localhost:3000/auth/google']
+      }
+    })
   ],
   providers: [
     {
