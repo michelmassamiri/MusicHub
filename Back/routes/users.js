a74b48f719042653b039a52c9a95f89fb463dc5d
Michel Massamiri <massamiri.michel@gmail.com>
2018-12-10 00:24:56 +0100

    API for User : TODO

--- Back/routes/users.js
+++ Back/routes/users.js
@@ -1,9 +1,29 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const jwt = require('express-jwt');
+
+function getTodos(jwtString)
+{
+    const token = jwt.decode(jwtString);
+    if( Date.now() < token.nbf*1000) {
+        throw new Error('Token not yet valid');
+    }
+    if( Date.now() > token.exp*1000) {
+        throw new Error('Token has expired');
+    }
+    if( token.iss != 'todoapi') {
+        throw new Error('Token not issued here');
+    }
+
+    const userID = token.uid;
+
+    return 'It works';
+}
 
 /* GET users listing. */
-router.get('/users', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function(req, res, next) {
+    const token = req.user.userID;
+    res.send({token});
 });
 
 module.exports = router;
