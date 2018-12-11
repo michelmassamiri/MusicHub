1dc6e2be625181fca1e31ec57eb9a1b836a431e3
Michel Massamiri <massamiri.michel@gmail.com>
2018-12-11 17:48:44 +0100

    Add Login Guard

---
+++ Front/Front/src/app/guards/auth.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router} from '@angular/router';
+import { Observable } from 'rxjs';
+import {LoginService} from "../services/login.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router, private loginService: LoginService) {
+
+  }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+      if(this.loginService.loggedIn) {
+        return true;
+      }
+
+      this.router.navigate(['login'])
+        .catch(()=> console.error('ERROR : Impossible de vous rediriger'));
+      return false;
+  }
+}
