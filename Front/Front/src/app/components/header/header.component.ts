6e4af5a9aa82a4e1177a277dfc0a0baa0d26a316
Michel Massamiri <massamiri.michel@gmail.com>
2018-12-12 13:46:43 +0100

    Update the header and Add sign out button

--- Front/Front/src/app/components/header/header.component.ts
+++ Front/Front/src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {LoginService} from "../../services/login.service";
+import {User} from "../../entity/User";
 
 @Component({
   selector: 'app-header',
@@ -7,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private loginService: LoginService
+  ) { }
 
   ngOnInit() {
   }
