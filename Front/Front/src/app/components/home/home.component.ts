d4dcaeeb9fe262d6844cc3800330585926e58648
Michel Massamiri <massamiri.michel@gmail.com>
2018-12-16 15:21:35 +0100

    Playlists Component

--- Front/Front/src/app/components/home/home.component.ts
+++ Front/Front/src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {PlaylistsService} from "../../services/playlists.service";
+import {ImportFromYoutubeService} from "../../services/import-from-youtube.service";
+import {Playlist} from "../../entity/Playlist";
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  playlists: Playlist[] = new Array<Playlist>();
 
-  constructor() { }
+  constructor(
+    private playlistsService: PlaylistsService,
+    private importYoutubePlaylist: ImportFromYoutubeService
+  ) {}
 
   ngOnInit() {
+
+  }
+
+  importFromYoutube() {
+    this.importYoutubePlaylist.importPlaylists()
+      .then((result) => {
+        this.playlistsService.importUserPlaylists(result)
+          .subscribe(
+            playlists => {
+              this.playlists.concat(playlists);
+            },
+              error => console.error(error)
+          );
+      });
   }
 
 }
