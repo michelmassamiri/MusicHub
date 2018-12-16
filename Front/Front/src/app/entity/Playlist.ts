e25002eda5b1d3f19d7f1863d14412c55a8dcc9c
Michel Massamiri <massamiri.michel@gmail.com>
2018-12-16 15:21:06 +0100

    Add Playlist Enity : Angular

---
+++ Front/Front/src/app/entity/Playlist.ts
@@ -0,0 +1,15 @@
+export class Playlist {
+  title: string;
+  genre: string;
+  link: string;
+  thumbnail: string;
+  user_id: string;
+
+  constructor(){
+    this.title = '';
+    this.genre = '';
+    this.link = '';
+    this.thumbnail = '';
+    this.user_id = '';
+  }
+}
