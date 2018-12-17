896883bd3c7a257f9897a8816bfed20d0e220c95
Michel Massamiri <massamiri.michel@gmail.com>
2018-12-17 17:45:48 +0100

    Add UpsertMethod to Model : Back end

--- Back/models/playlistsModel.js
+++ Back/models/playlistsModel.js
@@ -30,6 +30,50 @@ playlistsSchema.options.toJSON = {
     }
 };
 
+playlistsSchema.statics.insertOrUpdateFromYoutube = async function (playlists, userId, cb) {
+    let playlistsToUpdate = [];
+    let playlistsToInsert = [];
+    let result = [];
+    let error = null;
+
+    const criteria = playlists.map(elem => elem.link);
+    await this.find({link: {$in: criteria}, user_id:  mongoose.Types.ObjectId(userId)})
+        .exec(function (err, dbPlaylists) {
+            playlistsToUpdate = playlistsToUpdate.concat(dbPlaylists);
+            playlistsToInsert = playlistsToUpdate.filter(elemToUpdate => {
+                playlistsToInsert.forEach(elemToInsert => {
+                    return elemToUpdate.link !== elemToInsert;
+                });
+            });
+        });
+
+    if(playlistsToInsert.length !== 0) {
+        this.insertMany(playlistsToInsert, function (err, res) {
+           if(err) {
+               console.error(err);
+               return cb(err, res);
+           }
+           else {
+               playlistsToInsert = res;
+           }
+        });
+    }
+
+    playlistsToUpdate.forEach(async function (item) {
+        await this.updateOne({ 'link': item.link }, item, function (err, response) {
+            if(err) {
+                console.error(err);
+                error = err;
+            }
+            else {
+                result.push(response);
+            }
+        });
+    });
+
+    return cb(error, result.concat(playlistsToInsert));
+};
+
 const PlayList = module.exports = mongoose.model('PlayList', playlistsSchema);
 module.exports.get = function (callback, limit) {
     PlayList.find(callback).limit(limit);
