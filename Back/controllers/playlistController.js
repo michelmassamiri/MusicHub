6b574bdaebe405e64754275b9bc6123b958f9e8b
Michel Massamiri <massamiri.michel@gmail.com>
2018-12-19 23:55:19 +0100

    Backend Playlist Controller : importPlaylistsFromYoutube

--- Back/controllers/playlistController.js
+++ Back/controllers/playlistController.js
@@ -11,30 +11,31 @@ exports.createPlaylist = function (req, res, next) {
     const userId = req.user.userID;
     const playlists = req.body;
     if(Array.isArray(playlists)) {
-        res.json(importPlaylistsFromYoutube(playlists, userId));
+        importPlaylistsFromYoutube(playlists, userId)
+            .then((playlists) => {
+                res.json(playlists);
+            })
+            .catch(err => {
+               console.error(err);
+               next(err);
+            });
     }
-
 };
 
-function importPlaylistsFromYoutube(playlists, userId) {
+async function importPlaylistsFromYoutube(playlists, userId) {
     let userPlaylists = [];
     for(const item of playlists) {
         let playlist = new Playlist();
         playlist.title = item.snippet.title;
         playlist.link = 'https://www.youtube.com/playlist?list=' + item.id;
         playlist.thumbnail = item.snippet.thumbnails.high.url;
+        playlist.description = item.snippet.description;
+        playlist.nbItems = item.contentDetails.itemCount;
         playlist.user_id = userId;
         userPlaylists.push(playlist);
     }
 
-    Playlist.insertOrUpdateFromYoutube(userPlaylists, userId, function (err, playlists) {
-        if(err){
-            console.error(err);
-        }
-        else {
-            userPlaylists = playlists;
-        }
-    });
+    await Playlist.insertOrUpdateFromYoutube(userPlaylists, userId);
 
     return userPlaylists;
 }
