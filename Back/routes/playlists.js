3fbc9e046eedb4698d5dbf8cf43018af4807f95d
michelmassamiri <massamiri.michel@gmal.com>
2018-12-22 11:55:54 +0100

    Delete playlist Route API : Back end

--- Back/routes/playlists.js
+++ Back/routes/playlists.js
@@ -4,6 +4,7 @@ const playListController = require('../controllers/playlistController');
 
 router.get('', playListController.getAllPlaylists);
 router.post('', playListController.createPlaylist);
+router.delete('/:id', playListController.deletePlaylist);
 router.post('/import/youtube', playListController.importFromYoutube);
 
 module.exports = router;
