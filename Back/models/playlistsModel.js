b5f87d03f523ffb321bb2e14ee8026d56097c61c
Michel Massamiri <massamiri.michel@gmail.com>
2018-12-22 01:13:15 +0100

    Refactoring + create playlist : Back end

--- Back/models/playlistsModel.js
+++ Back/models/playlistsModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 ObjectId = mongoose.Schema.ObjectId;
+
 /* Setup the schema */
 const playlistsSchema = new mongoose.Schema({
    title: {
@@ -32,7 +33,7 @@ playlistsSchema.options.toJSON = {
         ret.id = ret._id;
         delete ret._id;
         delete ret.__v;
-        delete ret.id;
+        delete ret.user_id;
     }
 };
 
@@ -47,17 +48,26 @@ playlistsSchema.statics.insertOrUpdateFromYoutube = async function (playlists, u
     return await Promise.all(promiseArary);
 };
 
-async function getPlaylistsToUpdate(newPlaylists, userId, that) {
-    let playlistsToUpdate;
-    const playlistsLinks = newPlaylists.map(elem => elem.link);
+playlistsSchema.statics.insertPlaylist = async function (playlist, userId) {
+    const playlistToInsert = new PlayList({
+        title: playlist.title,
+        genre: playlist.genre,
+        link: playlist.link,
+        thumbnail: playlist.thumbnail,
+        description: playlist.description,
+        nbItems: playlist.nbItems,
+        user_id: mongoose.Types.ObjectId(userId)
+    });
 
-    playlistsToUpdate = await that.find({link: {$in: playlistsLinks}, user_id:  mongoose.Types.ObjectId(userId)})
-        .lean()
+    return await playlistToInsert.save()
+};
+
+playlistsSchema.statics.getUserPlaylists = async function (userId) {
+    return await this.find({user_id:  mongoose.Types.ObjectId(userId)})
         .exec();
+};
 
-    return playlistsToUpdate;
-}
-
+/* Private functions */
 function updateUserPlaylistByLink(playlist, userId, that){
     return that.updateOne({ link: playlist.link, user_id: mongoose.Types.ObjectId(userId)},
         {$set: { title: playlist.title, genre: playlist.genre,
@@ -65,6 +75,7 @@ function updateUserPlaylistByLink(playlist, userId, that){
         {upsert: true}).exec();
 }
 
+/* Export Playlist Model and DAO methods */
 const PlayList = module.exports = mongoose.model('PlayList', playlistsSchema);
 module.exports.get = function (callback, limit) {
     PlayList.find(callback).limit(limit);
