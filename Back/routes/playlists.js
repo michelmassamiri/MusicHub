0be15db2e079aae8fd1dd66ccd3e32d07ac58398
Michel Massamiri <massamiri.michel@gmail.com>
2018-12-27 12:28:39 +0100

    Add route for uploading photos

--- Back/routes/playlists.js
+++ Back/routes/playlists.js
@@ -5,6 +5,7 @@ const playListController = require('../controllers/playlistController');
 router.get('', playListController.getAllPlaylists);
 router.post('', playListController.createPlaylist);
 router.delete('/:id', playListController.deletePlaylist);
-router.post('/import/youtube', playListController.importFromYoutube);
 
+router.post('/import/youtube', playListController.importFromYoutube);
+router.post('/thumbnails/upload', playListController.uploadThumbnail);
 module.exports = router;
