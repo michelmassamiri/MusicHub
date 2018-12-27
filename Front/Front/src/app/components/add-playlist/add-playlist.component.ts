c0f75f7164d6b1d7bac014d7f86474e24d36ccc0
Michel Massamiri <massamiri.michel@gmail.com>
2018-12-27 12:30:49 +0100

    Adding the file uploader feature

--- Front/Front/src/app/components/add-playlist/add-playlist.component.ts
+++ Front/Front/src/app/components/add-playlist/add-playlist.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {PlaylistsService} from "../../services/playlists.service";
 import {Location} from "@angular/common";
 import {Playlist} from "../../entity/Playlist";
 import {ToastrService} from "ngx-toastr";
 import {Router} from "@angular/router";
+import {MUSICHUB_API, THUMBNAIL_UPLOAD_URI} from "../../../consts";
+import {LoginService} from "../../services/login.service";
+import {FileUploader} from 'ng2-file-upload/ng2-file-upload';
 
 @Component({
   selector: 'app-add-playlist',
@@ -13,15 +16,28 @@ import {Router} from "@angular/router";
 export class AddPlaylistComponent implements OnInit {
   genres: string[] = ['Pop', 'Rock', 'Jazz', 'Latino', 'R&B', 'Rap', 'HipHop', 'Techno', 'Metal'];
   playlist: Playlist = new Playlist();
+  uploader: FileUploader;
+  @ViewChild('selectedPicture') selectedPicture: any;
 
   constructor(
     private playlistService: PlaylistsService,
     private location: Location,
     private tosterService: ToastrService,
-    private router: Router
-  ) {}
+    private router: Router,
+    private loginService: LoginService
+  ) {
+    this.uploader = new FileUploader({
+      url: THUMBNAIL_UPLOAD_URI,
+      authToken: 'Bearer' + ' ' + this.loginService.currentUserValue.token,
+      allowedMimeType: ['image/png', 'image/jpeg', 'image/jpg'],
+    });
+  }
 
   ngOnInit() {
+    this.uploader.onAfterAddingFile = (file)=> { file.withCredentials = false; };
+    this.uploader.onCompleteItem = (item:any, response:any, status:any, headers:any) => {
+      this.playlist.thumbnail = MUSICHUB_API + '/uploads/' + JSON.parse(response);
+    };
   }
 
   addPlaylist() {
@@ -40,6 +56,11 @@ export class AddPlaylistComponent implements OnInit {
 
   cancel() {
     this.location.back();
+  }
+
+  removePhoto(item) {
+    this.uploader.removeFromQueue(item);
+    this.selectedPicture.nativeElement.value = '';
   }
 
 }
