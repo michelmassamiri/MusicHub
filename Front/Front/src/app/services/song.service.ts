2cfc4177d3bfaa474c5290d42535d1e3bf14808d
Michel Massamiri <massamiri.michel@gmail.com>
2019-01-13 19:35:22 +0100

    Add CRUD methods for Song : Angular

--- Front/Front/src/app/services/song.service.ts
+++ Front/Front/src/app/services/song.service.ts
@@ -15,6 +15,20 @@ export class SongService {
     return this.http.get<Song[]>(SONGS_URI, {params: new HttpParams().set('playlistId', playlistId)});
   }
 
+  public deleteSongForPlaylist(playlistId: string, songId: string): Observable<Song> {
+    return this.http.delete<Song>(SONGS_URI +"/" + songId,
+      {params: new HttpParams().set('playlistId', playlistId)});
+  }
+
+  public createSongForPlaylist(playlistId: string, song: Song): Observable<Song> {
+    return this.http.post<Song>(SONGS_URI, song, {params: new HttpParams().set('playlistId', playlistId)});
+  }
+
+  public updateSongForPlaylist(playlistId: string, songId: string, args: any): Observable<Song> {
+    return this.http.put<Song>(SONGS_URI + "/" + songId, args,
+      {params: new HttpParams().set('playlistId', playlistId)});
+  }
+
   public importYoutubeSongs(songs: Object[], playlistId: string): Observable<Song[]> {
     return this.http.post<Song[]>(IMPORTSONGS + '/youtube', songs,
       {params: new HttpParams().set('playlistId', playlistId)});
