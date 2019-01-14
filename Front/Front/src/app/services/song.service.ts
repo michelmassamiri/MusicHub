2a9a60a5943be3b15c3d1b1c8830320f832bbc88
Michel Massamiri <massamiri.michel@gmail.com>
2019-01-14 16:48:25 +0100

    Update the Song Service

--- Front/Front/src/app/services/song.service.ts
+++ Front/Front/src/app/services/song.service.ts
@@ -3,6 +3,7 @@ import {HttpClient, HttpParams} from "@angular/common/http";
 import {Observable} from "rxjs/index";
 import {Song} from "../entity/Song";
 import {IMPORTSONGS, SONGS_URI} from "../../consts";
+import {map} from "rxjs/internal/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -15,22 +16,42 @@ export class SongService {
     return this.http.get<Song[]>(SONGS_URI, {params: new HttpParams().set('playlistId', playlistId)});
   }
 
-  public deleteSongForPlaylist(playlistId: string, songId: string): Observable<Song> {
-    return this.http.delete<Song>(SONGS_URI +"/" + songId,
-      {params: new HttpParams().set('playlistId', playlistId)});
+  public deleteSongForPlaylist(songId: string): Observable<Song> {
+    return this.http.delete<Song>(SONGS_URI +"/" + songId);
   }
 
-  public createSongForPlaylist(playlistId: string, song: Song): Observable<Song> {
-    return this.http.post<Song>(SONGS_URI, song, {params: new HttpParams().set('playlistId', playlistId)});
+  public createSongForPlaylist(song: Song): Observable<Song> {
+    return this.http.post<Song>(SONGS_URI, song);
   }
 
-  public updateSongForPlaylist(playlistId: string, songId: string, args: any): Observable<Song> {
-    return this.http.put<Song>(SONGS_URI + "/" + songId, args,
-      {params: new HttpParams().set('playlistId', playlistId)});
+  public updateSongForPlaylist(songId: string, args: any): Observable<Song> {
+    return this.http.put<Song>(SONGS_URI + "/" + songId, args);
   }
 
   public importYoutubeSongs(songs: Object[], playlistId: string): Observable<Song[]> {
     return this.http.post<Song[]>(IMPORTSONGS + '/youtube', songs,
+      {params: new HttpParams().set('playlistId', playlistId)});
+  }
+
+  public getAllSongsByUser(): Observable<Song[]> {
+    return this.http.get<Song[]>(SONGS_URI).pipe(
+      map((songs: Song[])=> {
+        const result: Song[] = [];
+        const map = new Map();
+
+        for(const song of songs) {
+          if(!map.has(song.link)) {
+            map.set(song.link, true);
+            result.push(song);
+          }
+        }
+        return result;
+      })
+    );
+  }
+
+  public importAPISongs(songs: Song[], playlistId: string): Observable<Song[]> {
+    return this.http.post<Song[]>(IMPORTSONGS + '/api', songs,
       {params: new HttpParams().set('playlistId', playlistId)});
   }
 }
